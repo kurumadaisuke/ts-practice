c42aafa7a638bbae7bf30a23ab305cc2d305629e
kurumadaisuke <kuruma0909@gmail.com>
2024-08-11 13:15:03 +0900

    リテラル型

--- sample.ts
+++ sample.ts
@@ -8,5 +8,12 @@ let positveInfnity: number = Infinity; // 無限大
 // string型
 let firstName: string = "Alice";
 let greetiong = "Hello"; // 型推論
-let Message = "my name is ${firstName}" // 型推論
+let Message = "my name is ${firstName}"; // 型推論
+
+// boolean型
+let lsCompleted: boolean = false;
+let isValid = true; //型推論
+
+// リテラル型
+const auditAge = 18; // リテラル型（18と定義 再代入不可）
 
