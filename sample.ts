3520c44921eca4f5fd0b4a7c57237d55c44ab8e8
kurumadaisuke <kuruma0909@gmail.com>
2024-08-11 13:04:35 +0900

    firstcommit

---
+++ sample.ts
@@ -0,0 +1,12 @@
+// number型
+let age: number = 22;
+let height = 180.5;
+let hex: number = 0xff; // 16進数
+let notNumber: number = NaN; // 非数
+let positveInfnity: number = Infinity; // 無限大
+
+// string型
+let firstName: string = "Alice";
+let greetiong = "Hello"; // 型推論
+let Message = "my name is ${firstName}" // 型推論
+
