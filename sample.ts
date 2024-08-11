b10bfd701037826581d5b3e48d32be33d78668ce
kurumadaisuke <kuruma0909@gmail.com>
2024-08-11 14:00:21 +0900

    オブジェクト型

--- sample.ts
+++ sample.ts
@@ -17,3 +17,33 @@ let isValid = true; //型推論
 // リテラル型
 const auditAge = 18; // リテラル型（18と定義 再代入不可）
 
+// ユニオン型
+let id: number | string;
+let role: number | string | boolean;
+
+// 型エイリアス
+type Role = number | string; // 型エイリアスを定義
+let firstRole: Role; // 型エイリアスを利用
+
+// オブジェクト型
+let book: {
+  title: string;
+  author: string;
+  published: number;
+} = {
+  title: "こころ",
+  author: "夏目漱石",
+  published: 1914,
+};
+
+type Book = {
+  title: string;
+  author: string;
+  published: number;
+};
+
+const book_2: Book = {
+  title: "こころ",
+  author: "夏目漱石",
+  published: 1914,
+};
